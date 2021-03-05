408d189d8ebfacc1c6d43fffe5d483aba683f915
Ahern, David <david.ahern@dell.com>
2021-03-05 16:08:56 +0000

    module 1

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,11 +13,31 @@ class Home extends StatelessWidget {
               color: Colors.deepPurple,
               child: Center(
                 child: Text(
-                  "Hello Flutter 12345",
+                  sayHello(),
                   textDirection: TextDirection.ltr,
                   style: TextStyle(color: Colors.white),
                 ),
               ),
             )));
+  }
+
+  String sayHello() {
+    String hello;
+    DateTime now = DateTime.now();
+    int hour = now.hour;
+    int minute = now.minute;
+
+    if (hour < 12) {
+      hello = "Good Morning";
+    } else if (hour < 18) {
+      hello = "Good Afternoon";
+    } else {
+      hello = "Good Evening";
+    }
+
+    String minutes =
+        (minute < 10) ? "0" + minute.toString() : minute.toString();
+
+    return "It's now" + hour.toString() + ":" + minutes + "\n" + hello;
   }
 }
